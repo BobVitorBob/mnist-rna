0352ad070621f8a787476cbafa5ff0e9cd48bab7
BobVitorBob <vitordecastrosilva@gmail.com>
2020-09-28 12:34:49 -0300

    atualizei o .py e arrumei um bug na rede

--- RNA_Multicamada.py
+++ RNA_Multicamada.py
@@ -1,12 +1,8 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-import numpy as np
+import cupy as np
 import pandas as pd
-from IPython.display import clear_output
+import time
 
 
-# %%
 class Dense():
 
     def __init__(self, n_neurons):
@@ -21,7 +17,7 @@ class Dense():
 class Sequential():
 
     def __init__(self, input_size):
-        self.input_size = 4
+        self.input_size = input_size
         self.weights = []
         self.bias = []
         self.layers = []
@@ -35,14 +31,14 @@ class Sequential():
             print("------------------------------------------------------------------------------------------------")
             print(layer)
 
-    def compile(self):
-        self.weights.append(np.random.rand( self.input_size, self.layers[0].neurons.shape[0] ))
+    def compile(self, error_function, d_error_function):
+        self.weights.append(np.random.rand( self.input_size, self.layers[0].neurons.shape[0] ) - 0.5)
         self.bias.append(np.random.rand(self.layers[0].neurons.shape[0]))
         self.error_function = erro_quadratico
         self.d_error_function = d_erro_quadratico
         i = 0
         for layer in range(len(self.layers) - 1):
-            self.weights.append( np.random.rand( self.layers[i].neurons.shape[0], self.layers[i + 1].neurons.shape[0] ) )
+            self.weights.append( np.random.rand( self.layers[i].neurons.shape[0], self.layers[i + 1].neurons.shape[0] ) - 0.5)
             self.bias.append( np.random.rand(self.layers[i + 1].neurons.shape[0]) )
             i+=1
         
@@ -59,15 +55,20 @@ class Sequential():
             self.layers[i].neurons = self.layers[i].activation_function((self.layers[i - 1].neurons @ self.weights[i]) + self.bias[i])  
         return self.layers[-1].neurons
 
-    def backpropagation(self, X, Y, lr):
+    def backpropagation(self, X, Y, lr, show_info=False):
         for i in reversed(range(len(self.weights))):
+
             if i == len(self.weights) - 1:
-                erro = self.d_error_function(self.layers[i].neurons, Y)
+                erro = self.d_error_function(self.layers[-1].neurons, Y)
             else:
+                # print('pesos', self.weights[i + 1].shape)
+                # print('delta.T',delta.T.shape)
+                # print('(self.weights[i + 1] @ delta.T).T', erro.shape)
                 erro = (self.weights[i + 1] @ delta.T).T
+                # input()
 
             delta = erro * self.layers[i].d_activation_function(self.layers[i].neurons)
-            
+
             if i == 0:
                 dw = delta.T @ X
             else:
@@ -80,22 +81,68 @@ class Sequential():
         self.feedforward(X)
         return self.layers[-1].neurons
 
-    def fit(self, X, Y, X_val, Y_val, epochs=10, lr=0.01):
+    def fit(self, X, Y, X_val=[], Y_val=[], epochs=10, lr=0.01, stop_after=-1):
         """
-        Função de treino. Faz o treino sobre os conjuntos X e Y por epochs épocas. Após cada época o modelo               calcula o erro em cima dos conjuntos de validação. Após finalizar o treino, o modelo retorna os pesos que         tiveram o menor erro.
+        Função de treino. Faz o treino sobre os conjuntos X e Y por epochs épocas. Após cada época o modelo calcula o erro em cima dos conjuntos de validação. Após finalizar o treino, o modelo retorna os pesos que tiveram o menor erro.
         """
         menor_erro = 100_000
+        if stop_after < 0:
+            stop_after = epochs
+        if len(X_val) == 0:
+            X_val = X
+            Y_val = Y
+
+        start = time.time()
+        no_improvement = 0
+        lr = lr/X.shape[0]
         for i in range(epochs):
             self.feedforward(X)
-            self.backpropagation(X, Y, lr)
-            erro = sum(sum(self.error_function(self.predict(X_val), Y_val))/Y_val.shape[0])/Y_val.shape[1]
+            
+
+            if i % 30 == 0:
+                self.backpropagation(X, Y, lr, True)
+            else:
+                self.backpropagation(X, Y, lr, False)
+
+            validation = self.predict(X_val)
+
+            erro = np.mean(self.error_function(validation, Y_val))
+
             if erro < menor_erro:
+                no_improvement = 0
+                melhor_iteracao = i
                 menor_erro = erro
                 melhores_pesos = self.weights
-        return melhores_pesos
+                melhores_bias = self.bias
+            else:
+                no_improvement+=1
+                if no_improvement == stop_after:
+                    break
+
+            if i % 30 == 0:
+                accuracy = 0
+                for val in range(validation.shape[0]):
+                    if validation[val].argmax() == Y_val[val].argmax():
+                        accuracy+=1
+                accuracy = accuracy/Y_val.shape[0]
+                complete = str((i/epochs)*100)
+                acc = str(accuracy * 100)
+                print(complete+"% completo")
+                print("Melhor Iteração:", melhor_iteracao)
+                print("Menor Erro", menor_erro)
+                print("Acurácia até agora:", acc + "%")
+                
+
+        complete = str((i/epochs)*100)
+        acc = str(accuracy * 100)
+        print(complete+"% completo")
+        print("Melhor Iteração:", melhor_iteracao)
+        print("Menor Erro", menor_erro)
+        print("Acurácia até agora:", acc + "%")
+        
+        return melhores_pesos, melhores_bias
 
 
-# %%
 def sigmoid(x):
     return 1/(1+np.e**-x)
 
@@ -103,92 +150,87 @@ def d_sigmoid(sig_x):
     return sig_x*(1-sig_x)
 
 def d_erro_quadratico(Y, D):
-    return (2*Y)-(2*D)
+    return 2*(Y - D)
 
 def erro_quadratico(Y, D):
     return (D - Y)**2
 
+def erro_absoluto(Y, D):
+    return np.abs(D - Y)
 
-# %%
-# Pré processamento
-dataset = pd.read_csv("iris.csv", header=None)
+def d_erro_absoluto(Y, D):
+    return 
 
-X_train = pd.concat([dataset[0:30], dataset[50:80], dataset[100:130]]).sample(frac=1)
-X_val = pd.concat([dataset[30:40], dataset[80:90], dataset[130:140]]).sample(frac=1)
-X_test = pd.concat([dataset[40:50], dataset[90:100], dataset[140:150]]).sample(frac=1)
 
-Y_train = X_train.pop(4)
-Y_val = X_val.pop(4)
-Y_test = X_test.pop(4)
-
+dataset = pd.read_csv("dataset/MNIST.csv", header=None,)
+X_train = dataset.sample(frac=0.70)
+X_val = dataset.sample(frac=0.20)
+X_test = dataset.sample(frac=0.10)
+Y_train = X_train.pop(784)
+Y_val = X_val.pop(784)
+Y_test = X_test.pop(784)
 X_train = np.array(X_train)
 X_val = np.array(X_val)
 X_test = np.array(X_test)
+aaaa = {'zero': 0, 'um': 1, 'dois': 2, 'tres': 3, 'quatro': 4, 'cinco': 5, 'seis': 6, 'sete': 7, 'oito': 8, 'nove': 9}
+
+Y_train = np.array([aaaa[num] for num in Y_train])
+Y_val = np.array([aaaa[num] for num in Y_val])
+Y_test = np.array([aaaa[num] for num in Y_test])
+
+new_Y_train = []
+for ans in Y_train:
+    vec = [0,0,0,0,0,0,0,0,0,0]
+    vec[ans.get()] += 1
+    new_Y_train.append(vec)
+Y_train = np.array(new_Y_train)
+
+new_Y_val = []
+for ans in Y_val:
+    vec = [0,0,0,0,0,0,0,0,0,0]
+    vec[ans.get()] += 1
+    new_Y_val.append(vec)
+Y_val = np.array(new_Y_val)
+
+new_Y_test = []
+for ans in Y_test:
+    vec = [0,0,0,0,0,0,0,0,0,0]
+    vec[ans.get()] += 1
+    new_Y_test.append(vec)
+Y_test = np.array(new_Y_test)
 
 
-Y_train = np.array([ [1, 0, 0] if y == 'Iris-setosa' else [0, 1, 0] if y == 'Iris-versicolor' else [0, 0, 1] for y in Y_train])
-Y_val = np.array([ [1, 0, 0] if y == 'Iris-setosa' else [0, 1, 0] if y == 'Iris-versicolor' else [0, 0, 1] for y in Y_val])
-Y_test = np.array([ [1, 0, 0] if y == 'Iris-setosa' else [0, 1, 0] if y == 'Iris-versicolor' else [0, 0, 1] for y in Y_test])
+# Configs que deram certo:
+# 30, 25, 20, 10, lr=1, epochs=10_000, 97% depois de 2 fits
+# 30, 25, 20, 15, 10, lr=1, epochs=10_000, 96.15% depois de 1 fit (melhor rate parece)
+# 20, 20, 20, 10, lr=1, epochs=10_000, 95.8% depois de 1 fit
+# 35, 30, 25, 20, 15, 10, lr=1, epochs=10_000, 94.39% depois de 1 fit (Parece que melhora com mais treino)
+# 35, 30, 20, 20, 15, 10, lr=1, epochs=10_000, 94.39% depoisa de 1 fit (Parece que melhora com mais treino)
+nn1 = Sequential(784)
+nn1.add_layer(Dense(30))
+nn1.add_layer(Dense(25))
+nn1.add_layer(Dense(10))
+nn1.compile(erro_quadratico, d_erro_quadratico)
 
 
-# %%
-# Criação das redes
-nn1 = Sequential(4)
-nn1.add_layer(Dense(3))
-nn1.compile()
-nn2 = Sequential(4)
-nn2.add_layer(Dense(10))
-nn2.add_layer(Dense(3))
-nn2.compile()
-nn3 = Sequential(4)
-nn3.add_layer(Dense(5))
-nn3.add_layer(Dense(5))
-nn3.add_layer(Dense(3))
-nn3.compile()
+nn1.weights, nn1.bias = nn1.fit(X_train, Y_train, X_val=X_val, Y_val=Y_val, epochs=50_000, lr=1, stop_after=1000)
 
 
-# %%
-# Treino
-nn1.weights = nn1.fit(X_train, Y_train, X_val, Y_val, 10_000, lr=0.01)
-nn2.weights = nn2.fit(X_train, Y_train, X_val, Y_val, 10_000, lr=0.01)
-nn3.weights = nn3.fit(X_train, Y_train, X_val, Y_val, 10_000, lr=0.01)
-
-
-# %%
-# Cálculo das métricas
 vp1, vn1, fp1, fn1 = [], [], [], []
-vp2, vn2, fp2, fn2 = [], [], [], []
-vp3, vn3, fp3, fn3 = [], [], [], []
 
-for c in range(3):
+for c in range(Y_test.shape[1]):
     vp1.append(0)
     vn1.append(0)
     fp1.append(0)
     fn1.append(0)
-    vp2.append(0)
-    vn2.append(0)
-    fp2.append(0)
-    fn2.append(0)
-    vp3.append(0)
-    vn3.append(0)
-    fp3.append(0)
-    fn3.append(0)
 
 output_nn1 = nn1.predict(X_test)
-output_nn2 = nn2.predict(X_test)
-output_nn3 = nn3.predict(X_test)
+prediction = []
+for kick in output_nn1:
+    prediction.append([0,0,0,0,0,0,0,0,0,0])
+    prediction[-1][kick.argmax().get()]+=1 
 
-output_nn1 = np.array([ [1, 0, 0] if n.argmax() == 0 else [0, 1, 0] if n.argmax() == 1 else [0, 0, 1] for n in output_nn1])
-
-output_nn2 = np.array([ [1, 0, 0] if n.argmax() == 0 else [0, 1, 0] if n.argmax() == 1 else [0, 0, 1] for n in output_nn2])
-
-output_nn3 = np.array([ [1, 0, 0] if n.argmax() == 0 else [0, 1, 0] if n.argmax() == 1 else [0, 0, 1] for n in output_nn3])
-
-a1 = 0
-a2 = 0
-a3 = 0
-
-for predict_nn1, predict_nn2, predict_nn3, expected in zip(output_nn1, output_nn2, output_nn3, Y_test):
+for predict_nn1, expected in zip(prediction, Y_test):
     for n in range(expected.shape[0]):
 
         if predict_nn1[n] == expected[n]:
@@ -202,28 +244,6 @@ for predict_nn1, predict_nn2, predict_nn3, expected in zip(output_nn1, output_nn
             else:
                 fn1[n]+=1
 
-        if predict_nn2[n] == expected[n]:
-            if expected[n] == 0:
-                vn2[n]+=1
-            else:
-                vp2[n]+=1
-        else:
-            if expected[n] == 0:
-                fp2[n]+=1
-            else:
-                fn2[n]+=1
-
-        if predict_nn3[n] == expected[n]:
-            if expected[n] == 0:
-                vn3[n]+=1
-            else:
-                vp3[n]+=1
-        else:
-            if expected[n] == 0:
-                fp3[n]+=1
-            else:
-                fn3[n]+=1
-
 
 
 vn1 = np.array(vn1)
@@ -231,40 +251,23 @@ vp1 = np.array(vp1)
 fn1 = np.array(fn1)
 fp1 = np.array(fp1) 
 
-vn2 = np.array(vn2)
-vp2 = np.array(vp2)
-fn2 = np.array(fn2)
-fp2 = np.array(fp2) 
-
-vn3 = np.array(vn3)
-vp3 = np.array(vp3)
-fn3 = np.array(fn3)
-fp3 = np.array(fp3)
 
 precision1 = vp1/(vp1+fp1)
 recall1 = vp1/(vp1+fn1)
 accuracy1 = sum(vp1)/sum(vp1 + fn1)
 
-precision2 = vp2/(vp2+fp2)
-recall2 = vp2/(vp2+fn2)
-accuracy2 = sum(vp2)/sum(vp2 + fn2)
 
-precision3 = vp3/(vp3+fp3)
-recall3 = vp3/(vp3+fn3)
-accuracy3 = sum(vp3)/sum(vp3 + fn3)
+a = nn1.predict(X_train)
 
 
-# %%
-print('Precision 1 camada: ', precision1)
-print('Recall 1 camada: ', recall1)
-print('Accuracy 1 camada: ', accuracy1)
+print('Precision: ', precision1)
+print('Recall: ', recall1)
+print('Accuracy: ', accuracy1)
 
-print('Precision 2 camadas: ', precision2)
-print('Recall 2 camadas: ', recall2)
-print('Accuracy 2 camadas: ', accuracy2)
 
-print('Precision 3 camadas: ', precision3)
-print('Recall 3 camadas: ', recall3)
-print('Accuracy 3 camadas: ', accuracy3)
+a = [max(b) for b in a] 
+
+
+a
 
 
